b0a1b2b34a77df079948f32330af4932646da80f
Alexey Rybak <arybak@meta.com>
2025-07-15 12:12:48 -0700

    fix: run script

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 # Run generate.py with GPT-4o model
 echo "Running generate.py with GPT-4o model..."
 OPENAI_API_KEY=$(cat ~/.openai/key) \
-./generate.py conversation_samples.yaml gpt-4o --output golden.jsonl
+./generate.py sample_conversations.yaml gpt-4o --output golden.jsonl
 
 
 # Run generate.py with Llama-4-Maverick model
 echo "Running generate.py with Llama-4-Maverick model..."
 OPENAI_API_KEY=$(cat ~/.llama/api/key) \
 BASE_URL=https://api.llama.com/compat/v1 \
-./generate.py conversation_samples.yaml Llama-4-Maverick-17B-128E-Instruct-FP8 --output maverick.jsonl
+./generate.py sample_conversations.yaml Llama-4-Maverick-17B-128E-Instruct-FP8 --output maverick.jsonl
 
 # Run score.py with the two output files
 echo "Running score.py with the generated files..."
